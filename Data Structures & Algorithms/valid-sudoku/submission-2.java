2e0df12cb146f387cf584bd2e2fb8ea0686360fe
CherylSharma <124386102+TankBartBRO@users.noreply.github.com>
2026-05-19 16:01:27 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> set = new HashSet<>();
+        for (int i=0; i<9; i++) {
+            for (int j=0; j<9; j++) {
+                char val=board[i][j];
+                if (val != '.') {
+                    if (!set.add(val + " added in row " + i)||
+                        !set.add(val + " added in col " + j)||
+                        !set.add(val + " added in box " + i/3 + j/3))
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+}
